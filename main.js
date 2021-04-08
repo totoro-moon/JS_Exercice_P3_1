fc64e87566a31b6365a9eee28604490b4e156978
FLAVIE <flavie@MacBook-Pro-de-FLAVIE.local>
2021-04-08 13:55:08 +0200

    JS_Exercice_P3_1

---
+++ main.js
@@ -0,0 +1,14 @@
+var img = document.querySelector('img');
+img.addEventListener('mouseover', function () {
+   this.style.border = '4px solid pink';
+});
+img.addEventListener('mouseout', function () {
+   this.style.border = 'none';
+});
+
+
+
+
+
+
+
